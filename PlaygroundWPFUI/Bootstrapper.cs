834c20f9d080455529b947a9a03f3f125b528792
Gloria Zaubitzer <g.zaubitzer@gmail.com>
2019-06-03 15:19:55 +0200

    Set up caliburn.micro
    Set up caliburn.micro with instructions at
    https://github.com/Caliburn-Micro/Caliburn.Micro/tree/master/samples/setup/Setup.WPF

---
+++ PlaygroundWPFUI/Bootstrapper.cs
@@ -0,0 +1,20 @@
+﻿using Caliburn.Micro;
+using PlaygroundWPFUI.ViewModels;
+using System.Windows;
+
+namespace PlaygroundWPFUI
+{
+    public class Bootstrapper : BootstrapperBase
+    {
+
+        public Bootstrapper()
+        {
+            Initialize();
+        }
+
+        protected override void OnStartup(object sender, StartupEventArgs e)
+        {
+            DisplayRootViewFor<ShellViewModel>();
+        }
+    }
+}
---
+++ PlaygroundWPFUI/ViewModels/ShellViewModel.cs
@@ -0,0 +1,9 @@
+﻿using Caliburn.Micro;
+
+namespace PlaygroundWPFUI.ViewModels
+{
+    public class ShellViewModel : Screen
+    {
+
+    }
+}
