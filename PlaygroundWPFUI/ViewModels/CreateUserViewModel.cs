089044c38cf7829cd1f6ae6bcc49ba5f92d35133
Gloria Zaubitzer <g.zaubitzer@gmail.com>
2019-06-04 21:00:14 +0200

    Started implementing properties for CreateUserView

--- PlaygroundWPFUI/ViewModels/CreateUserViewModel.cs
+++ PlaygroundWPFUI/ViewModels/CreateUserViewModel.cs
@@ -3,10 +3,42 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Caliburn.Micro;
 
 namespace PlaygroundWPFUI.ViewModels
 {
-    class CreateUserViewModel
+    class CreateUserViewModel : Screen
     {
+        private string _username;
+        private string _firstname;
+        private string _lastname;
+
+
+        public string Username
+        {
+            get { return _username; }
+            set { _username = value; }
+        } 
+
+        public string Firstname
+        {
+            get { return _firstname; }
+            set
+            {
+                _firstname = value;
+                NotifyOfPropertyChange(() => Firstname);
+            }
+        }     
+
+        public string Lastname
+        {
+            get { return _lastname; }
+            set
+            { _lastname = value;
+                NotifyOfPropertyChange(() => Lastname);
+            }
+        }
+
+        
     }
 }
