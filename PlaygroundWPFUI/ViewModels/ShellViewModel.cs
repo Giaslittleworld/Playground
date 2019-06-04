afabe33023f940a1a1ae9899fdc669ce87d3b51d
Gloria Zaubitzer <g.zaubitzer@gmail.com>
2019-06-04 09:13:05 +0200

    Revert "Revert "Combining Names""
    This reverts commit d199c48d378c6576c430839c96568e36112437a5.

--- PlaygroundWPFUI/ViewModels/ShellViewModel.cs
+++ PlaygroundWPFUI/ViewModels/ShellViewModel.cs
@@ -1,9 +1,60 @@
 ﻿using Caliburn.Micro;
+using System;
 
 namespace PlaygroundWPFUI.ViewModels
 {
     public class ShellViewModel : Screen
     {
+        private string _firstName;
+        private string _lastName;
+        private string _fullName;
 
+        public string FirstName
+        {
+            get { return _firstName; }
+            set
+            {
+                _firstName = value;
+                NotifyOfPropertyChange(() => FirstName);
+                NotifyOfPropertyChange(() => CanCombine);
+            }
+        }
+
+        public string LastName
+        {
+            get { return _lastName; }
+            set
+            {
+                _lastName = value;
+                NotifyOfPropertyChange(() => LastName);
+                NotifyOfPropertyChange(() => CanCombine);
+            }
+        }
+
+
+        public string FullName
+        {
+            get { return _fullName; }
+            set {
+                _fullName = value;
+                NotifyOfPropertyChange(() => FullName);
+            }
+        }
+
+
+        public bool CanCombine
+        {
+            get
+            {
+                return FirstName?.Length > 0 && LastName?.Length > 0;
+            }
+        }
+
+        public void Combine()
+        {
+
+            FullName = $" { FirstName } { LastName } ";
+
+        }
     }
 }
