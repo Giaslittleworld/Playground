0cde8bd502062914454104fbd22ca91110227533
Gloria Zaubitzer <g.zaubitzer@gmail.com>
2019-06-04 09:47:04 +0200

    Changed ShellViewModel to Conductor
    Changed ShellViewModel to Conductor and switched CombineNames to new View to show on ShellViewModel.

---
+++ PlaygroundWPFUI/ViewModels/CombineNamesViewModel.cs
@@ -0,0 +1,65 @@
+﻿using Caliburn.Micro;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PlaygroundWPFUI.ViewModels
+{
+    public class CombineNamesViewModel : Screen
+    {
+        private string _firstName;
+        private string _lastName;
+        private string _fullName;
+
+        public string FirstName
+        {
+            get { return _firstName; }
+            set
+            {
+                _firstName = value;
+                NotifyOfPropertyChange(() => FirstName);
+                NotifyOfPropertyChange(() => CanCombine);
+            }
+        }
+
+        public string LastName
+        {
+            get { return _lastName; }
+            set
+            {
+                _lastName = value;
+                NotifyOfPropertyChange(() => LastName);
+                NotifyOfPropertyChange(() => CanCombine);
+            }
+        }
+
+
+        public string FullName
+        {
+            get { return _fullName; }
+            set
+            {
+                _fullName = value;
+                NotifyOfPropertyChange(() => FullName);
+            }
+        }
+
+
+        public bool CanCombine
+        {
+            get
+            {
+                return FirstName?.Length > 0 && LastName?.Length > 0;
+            }
+        }
+
+        public void Combine()
+        {
+
+            FullName = $" { FirstName } { LastName } ";
+
+        }
+    }
+}
--- PlaygroundWPFUI/ViewModels/ShellViewModel.cs
+++ PlaygroundWPFUI/ViewModels/ShellViewModel.cs
@@ -3,58 +3,11 @@ using System;
 
 namespace PlaygroundWPFUI.ViewModels
 {
-    public class ShellViewModel : Screen
+    public class ShellViewModel : Conductor<object>
     {
-        private string _firstName;
-        private string _lastName;
-        private string _fullName;
-
-        public string FirstName
-        {
-            get { return _firstName; }
-            set
-            {
-                _firstName = value;
-                NotifyOfPropertyChange(() => FirstName);
-                NotifyOfPropertyChange(() => CanCombine);
-            }
-        }
-
-        public string LastName
-        {
-            get { return _lastName; }
-            set
-            {
-                _lastName = value;
-                NotifyOfPropertyChange(() => LastName);
-                NotifyOfPropertyChange(() => CanCombine);
-            }
-        }
-
-
-        public string FullName
-        {
-            get { return _fullName; }
-            set {
-                _fullName = value;
-                NotifyOfPropertyChange(() => FullName);
-            }
-        }
-
-
-        public bool CanCombine
-        {
-            get
-            {
-                return FirstName?.Length > 0 && LastName?.Length > 0;
-            }
-        }
-
-        public void Combine()
-        {
-
-            FullName = $" { FirstName } { LastName } ";
-
+        public ShellViewModel()
+        {            
+            ActivateItem(new CombineNamesViewModel());
         }
     }
 }
